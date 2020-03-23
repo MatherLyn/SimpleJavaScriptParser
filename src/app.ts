ddbcb1b5be5f827031e32eb137b2dd764d48f7dd
MatherLyn <2907681146@qq.com>
2020-03-24 00:05:23 +0800

    debug css-loader & style-lodaer

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import './index.css'
 import input from './input'
 import tokenizer from './tokenizer'
 import parser from './parser'
@@ -6,7 +5,3 @@ import parser from './parser'
 const tokens = tokenizer(input)
 
 const ast = parser(tokens)
-
-// import util from './util'
-// const a: Array<typeof util.Token> = []
-// console.log(a)
